6e0d879d78f709200466054b2df952ab3b06115a
Art Mills <art@hamcois.com>
2024-03-21 17:58:15 -0500

    Day 1 of 100 days done

---
+++ Day1/main.py
@@ -0,0 +1,11 @@
+# Welcome to Band Name Generator
+print("Welcome to Band Name Generator")
+# What's name of the city you grew up in?
+city = input("What's name of the city you grew up in?\n")
+# What's your pet's name?
+pet = input("What's your pet's name?\n")
+# Combine the name of their city and pet to get your band name.
+band_name = city + " " + pet
+# Your band name could be: city + pet name
+print("Your band name could be: " + band_name)
+
